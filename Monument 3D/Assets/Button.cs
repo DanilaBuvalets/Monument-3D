cd24e6af77f94bf0038bcc44410de8664c73154f
RTU MIREA <mirea@mirea.ru>
2019-12-20 10:38:54 +0300

    Update2

---
+++ Monument 3D/Assets/Button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] GameObject Menu;
+    [SerializeField] GameObject Rules;
+    public void Begin()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ProjectM");
+    }
+    public void Close()
+    {
+        Application.Quit();
+    }
+    public void Settings()
+    {
+        Menu.SetActive(false);
+        Rules.SetActive(true);
+    }
+    public void gotomenu()
+    {
+        Menu.SetActive(true);
+        Rules.SetActive(false);
+    }
+    public void Exittomenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Monument 3D/Assets/Exit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public void Exittomenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
